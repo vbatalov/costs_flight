5dd0a429f285735f11321635d55363f98260fd90
vbatalov <vbatalov1996@gmail.com>
2024-03-29 15:29:46 +0700

    test filter

--- app/Http/Controllers/SetCostFlightController.php
+++ app/Http/Controllers/SetCostFlightController.php
@@ -10,13 +10,57 @@ class SetCostFlightController extends Controller
 {
     public function handle(Request $request)
     {
+        return $this->filterSameLong($request->post());
 
-        foreach($request->all() as $key => $item) {
-            if($key == "pkkey") continue;
-
+        foreach ($request->post() as $key => $item) {
             UpdateCostJob::dispatch(items: $item, pkkey: $request->query->get("pkkey"));
         }
 
         return response("ok");
+    }
+
+    private function filterSameLong(array $items)
+    {
+        $duplicated = [];
+        $itemsForDelete = [];
+
+        foreach ($items as $key => $item) {
+            $cost = $item['cost'];
+            foreach ($items as $item2) {
+                if (
+                    /** Ищу все совпадения, кроме LONG. LONG должен отличаться */
+                    $item['pkkey'] == $item2['pkkey']
+                    and $item['AirlineAndFlight'] == $item2['AirlineAndFlight']
+                    and $item['date_flight'] == $item2['date_flight']
+                    and $item['cost'] == $item2['cost']
+                    and $item['long'] != $item2['long']
+
+                ) {
+                    // Если ключ $cost (цена) уже есть, добавляю LONG в массив
+                    if (isset($duplicated[$cost])) {
+                        // чтобы не было дублирования в массиве LONG'ов, проверяю, есть ли такой LONG уже в массиве с ценой
+                        // если нет, добавляю LONG
+                        if (!in_array( $item['long'], $duplicated[$cost])) {
+                            array_push($duplicated[$cost], $item['long']);
+                        }
+                    } else {
+                        // если ещё нет ключа $cost, создаю
+                        $duplicated[$cost] = [
+                            $item['long']
+                        ];
+                    }
+
+                    // собираю ключи для удаления из общего POST запроса.
+                    // Кортежи, которые не совпадают по LONG'у, я оставляю для обновления без "IN"
+                    $itemsForDelete[] = $key;
+                }
+            }
+        }
+
+        foreach ($itemsForDelete as $key) {
+            unset($items[$key]);
+        }
+
+        dd($duplicated, $items);
     }
 }
--- tests/Feature/CostTest.php
+++ tests/Feature/CostTest.php
@@ -62,8 +62,50 @@ class CostTest extends TestCase
                 $this->assertTrue(true);
             }
 
-            $this->assertDatabaseCount("tbl_costs", 0);
+
         }
 
+    }
+
+    public function test_detectSomeLong()
+    {
+        $post_data =
+            [
+                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "11.05.2024", "cost" => 422, "long" => 3], //0
+                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "11.05.2024", "cost" => 422, "long" => 4], //1
+                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "12.05.2024", "cost" => 351, "long" => 5], //2
+                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "12.05.2024", "cost" => 500, "long" => 6], //3
+                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "12.05.2024", "cost" => 500, "long" => 7], //4
+                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "11.05.2024", "cost" => 422, "long" => 8], //5
+                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "11.05.2024", "cost" => 422, "long" => 9], //6
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "17.05.2024", "cost" => 389, "long" => 9],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "18.05.2024", "cost" => 406, "long" => 10],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "19.05.2024", "cost" => 379, "long" => 11],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "20.05.2024", "cost" => 361, "long" => 12],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "21.05.2024", "cost" => 358, "long" => 13],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "22.05.2024", "cost" => 358, "long" => 14],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "23.05.2024", "cost" => 358, "long" => 15],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "24.05.2024", "cost" => 386, "long" => 16],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "25.05.2024", "cost" => 386, "long" => 17],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "26.05.2024", "cost" => 377, "long" => 18],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "27.05.2024", "cost" => 358, "long" => 19],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "28.05.2024", "cost" => 355, "long" => 20],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "29.05.2024", "cost" => 352, "long" => 21],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "30.05.2024", "cost" => 374, "long" => 22],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "31.05.2024", "cost" => 378, "long" => 23],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "01.06.2024", "cost" => 422, "long" => 24],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "01.06.2024", "cost" => 422, "long" => 25],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "01.06.2024", "cost" => 422, "long" => 26],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "02.06.2024", "cost" => 372, "long" => 27],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "02.06.2024", "cost" => 372, "long" => 28],
+//                ["pkkey" => "30814", "AirlineAndFlight" => "GDS005", "date_flight" => "02.06.2024", "cost" => 372, "long" => 29],
+        ];
+
+        $response = $this->post(uri: "/api/set_cost_flight?pkkey=30814",
+            data: $post_data)->withHeaders([
+            "Content-Type: application/json",
+        ]);
+
+        dd($response);
     }
 }
